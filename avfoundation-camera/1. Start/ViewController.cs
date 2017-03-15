aae399bf73f1e8746deb0a3b539dbdbe1f617761
Tiago Nunes <nunes_t@hotmail.com>
2017-03-15 16:48:14 +0000

    not private

---
+++ avfoundation-camera/1. Start/ViewController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+using UIKit;
+using Foundation;
+using AVFoundation;
+using System.Threading.Tasks;
+
+namespace Camera
+{
+	public partial class ViewController : UIViewController
+	{
+		bool flashOn = false;
+
+		public ViewController (IntPtr handle) : base (handle)
+		{
+		}
+
+		public override async void ViewDidLoad ()
+		{
+			base.ViewDidLoad ();
+			await AuthorizeCameraUse();
+		}
+
+		public override void DidReceiveMemoryWarning ()
+		{
+			base.DidReceiveMemoryWarning ();
+		}
+
+		async partial void TakePhotoButtonTapped (UIButton sender)
+		{
+
+		}
+
+		partial void SwitchCameraButtonTapped (UIButton sender)
+		{
+			
+		}
+
+		partial void FlashButtonTapped (UIButton sender)
+		{
+			
+		}
+
+		async Task AuthorizeCameraUse()
+		{
+			var authorizationStatus = AVCaptureDevice.GetAuthorizationStatus(AVMediaType.Video);
+
+			if (authorizationStatus != AVAuthorizationStatus.Authorized)
+			{
+				await AVCaptureDevice.RequestAccessForMediaTypeAsync(AVMediaType.Video);
+			}
+		}
+	}
+}
---
+++ testing/testing/ViewController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+using UIKit;
+
+namespace testing
+{
+	public partial class ViewController : UIViewController
+	{
+		protected ViewController(IntPtr handle) : base(handle)
+		{
+			// Note: this .ctor should not contain any initialization logic.
+		}
+
+		public override void ViewDidLoad()
+		{
+			base.ViewDidLoad();
+			// Perform any additional setup after loading the view, typically from a nib.
+
+			UILabel _helloLabel;
+
+			_helloLabel = new UILabel
+			{
+				Frame = new CoreGraphics.CGRect(50, 50, 120, 40),
+				Text = "TEST", TextColor = UIColor.Magenta
+			};
+
+			View.AddSubview(_helloLabel);
+		}
+
+		public override void DidReceiveMemoryWarning()
+		{
+			base.DidReceiveMemoryWarning();
+			// Release any cached data, images, etc that aren't in use.
+		}
+	}
+}
